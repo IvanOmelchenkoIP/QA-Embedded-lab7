3348ea2d65dc4fc1d738e79eafa6f26caf48972c
IvanOmelchenkoIP <io25112002@gmail.com>
2023-12-20 22:07:44 +0200

    Add test suite and regexp
    Add test suite and regex for doing lab

--- src/iperf_parser.py
+++ src/iperf_parser.py
@@ -1,1 +1,4 @@
 import re
+
+REGEXP = '\[\s*[0-9]*\]\s*[0-9\.-]*\s*(sec)\s*[0-9\.]*\s*[A-Z]?(Bytes)\s*[0-9\.]*\s*[A-Z]?(bits/sec)\s*[0-9]*\s*[0-9\.]*\s*[A-Z]?(Bytes)'
+KEYS = ('Interval', 'Transfer', 'Bitrate', 'Retr', 'Cwnd')
--- src/iperf_test.py
+++ src/iperf_test.py
@@ -1,1 +1,6 @@
 import iperf_parser
+
+class TestSuite:
+
+    def test_iperf3_client_connection(client):
+        return
