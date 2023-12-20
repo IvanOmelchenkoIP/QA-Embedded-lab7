d60facf90bcaf17de72f0f7ebff70020fb963ec8
IvanOmelchenkoIP <io25112002@gmail.com>
2023-12-20 22:01:24 +0200

    Add basic code
    Add basic code for structure

---
+++ src/conftest.py
@@ -0,0 +1,17 @@
+import paramiko
+import subprocess
+import pytest
+
+server_ip = ""
+password = ""
+username = ""
+
+
+@pytest.fixture(scope="function")
+def server():
+    return
+
+
+@pytest.fixture(scope="function")
+def client():
+    return
---
+++ src/iperf_test.py
@@ -0,0 +1,1 @@
+import iperf_parser
