58efb20c000d895c00083a51d19674a7c7b264fc
IvanOmelchenkoIP <io25112002@gmail.com>
2023-12-21 16:06:02 +0200

    Fixes
    Fixes and deletion of unneeded data

--- src/iperf_test.py
+++ src/iperf_test.py
@@ -4,12 +4,9 @@ import iperf_parser
 class TestSuite:
     def test_iperf3_client_connection(self, client):
         stdout, error, error_serv = client
-        res = "Result from client " + stdout
-        print(res)
-        assert not error
         assert not error_serv
+        assert not error
         dict = iperf_parser.parser(stdout)
-        assert len(dict)
         for value in dict:
             transfer = float(value["Transfer"])
             bitrate = float(value["Bitrate"])
